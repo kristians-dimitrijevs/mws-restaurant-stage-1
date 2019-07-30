890b9471b17c2edb8db3419db754e0e79a2851a1
Kristians Dimitrijevs <kristians.dimitrijevs@gmail.com>
2019-07-30 14:44:24 +0300

    Added ServiceWorker script and fixed indentation

---
+++ sw.js
@@ -0,0 +1,78 @@
+let CACHE_NAME = 'my-restaurant-app-1';
+let urlsToCache = [
+  '/',
+  'index.html',
+  'restaurant.html',
+  'css/styles.css',
+  'data/restaurants.json',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'js/sw-register.js',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Event install');
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+let staticCacheName = 'restaurant-static-v1';
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+			return cache.addAll([
+				'./',
+				'./index.html',
+				'./restaurant.html',
+				'./css/styles.css',
+				'./data/restaurants.json',
+				'./js/dbhelper.js',
+				'./js/main.js',
+				'./js/restaurant_info.js',
+				'./js/sw-register.js',
+				'./img/1.jpg',
+				'./img/2.jpg',
+				'./img/3.jpg',
+				'./img/4.jpg',
+				'./img/5.jpg',
+				'./img/6.jpg',
+				'./img/7.jpg',
+				'./img/8.jpg',
+				'./img/9.jpg',
+				'./img/10.jpg'
+			]);
+		})
+	);
+});
